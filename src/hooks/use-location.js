b7b6ec64a452c847b2482e35bba4272c11962cb0
Hồ Đặng Nam Tiến <onechampbrand@gmail.com>
2023-07-03 11:27:29 +0700

    Update use-location.js

--- src/hooks/use-location.js
+++ src/hooks/use-location.js
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 
 const useLocation = () => {
   const id = useSelector((state) => state.task.id);
-
+  const timeoutRef = useRef(null);
   const getLocation = () => {
     console.log("thuc thi task");
     // Geolocation.getCurrentPosition(
@@ -15,13 +15,16 @@ const useLocation = () => {
     //   (error) => {},
     //   { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     // );
-    setTimeout(getLocation, 15000);
+    timeoutRef.current = setTimeout(getLocation, 2000);
   };
 
   useEffect(() => {
     if (id) {
       getLocation();
     }
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
   }, [id]);
 };
 
