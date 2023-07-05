84384afdcd2e1ca149dbf5a008e36f495ed14025
Hồ Đặng Nam Tiến <onechampbrand@gmail.com>
2023-07-05 13:12:54 +0700

    remove saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
-import createSagaMiddleware from "redux-saga";
 import rootReducer from "./root-reducer";
-import rootSaga from "./root-saga";
 
 const persistConfig = {
   key: "root",
@@ -12,17 +10,13 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const sagaMiddleware = createSagaMiddleware();
-
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(sagaMiddleware),
+    }),
 });
 
 export default store;
 export const persistor = persistStore(store);
-
-sagaMiddleware.run(rootSaga);
